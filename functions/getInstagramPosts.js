01eba42142950564708122056f06c9b9c5fd233f
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-03-30 19:02:52 -0400

    updating

--- functions/getInstagramPosts.js
+++ functions/getInstagramPosts.js
@@ -1,33 +1,33 @@
 // functions/getInstagramPosts.js
 
-const axios = require("axios");
+// const axios = require("axios");
 
-exports.handler = async function (event, context) {
-  const { INSTAGRAM_ACCESS_TOKEN } = process.env;
+// exports.handler = async function (event, context) {
+//   const { INSTAGRAM_ACCESS_TOKEN } = process.env;
 
-  if (!INSTAGRAM_ACCESS_TOKEN) {
-    return {
-      statusCode: 500,
-      body: "INSTAGRAM_ACCESS_TOKEN is not set",
-    };
-  }
+//   if (!INSTAGRAM_ACCESS_TOKEN) {
+//     return {
+//       statusCode: 500,
+//       body: "INSTAGRAM_ACCESS_TOKEN is not set",
+//     };
+//   }
 
-  try {
-    const response = await axios.get(
-      `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,thumbnail_url,timestamp,username&access_token=${INSTAGRAM_ACCESS_TOKEN}`
-    );
-    const posts = response.data.data;
+//   try {
+//     const response = await axios.get(
+//       `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,thumbnail_url,timestamp,username&access_token=${INSTAGRAM_ACCESS_TOKEN}`
+//     );
+//     const posts = response.data.data;
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(posts),
-    };
-  } catch (error) {
-    console.error(error);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(posts),
+//     };
+//   } catch (error) {
+//     console.error(error);
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed to fetch Instagram posts" }),
-    };
-  }
-};
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: "Failed to fetch Instagram posts" }),
+//     };
+//   }
+// };
