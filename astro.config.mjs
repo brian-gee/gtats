d30d63040d11ba8f749d30176b7a154315efc592
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-03-30 18:53:01 -0400

    adding netlify, removing api folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
-import vercel from '@astrojs/vercel/serverless';
-
 import svelte from '@astrojs/svelte';
+
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), svelte()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
